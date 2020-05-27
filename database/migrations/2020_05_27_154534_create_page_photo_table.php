454771b9ca4dc13b972ef80bb75905a0d9181b2a
fabiofanta <f.fantacuzzi@gmail.com>
2020-05-27 17:46:20 +0200

    Add Migrations

---
+++ database/migrations/2020_05_27_154534_create_page_photo_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePagePhotoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('page_photo', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('page_photo');
+    }
+}
